41fa35fa3ae6f228f6780673530d3123397581af
Manfred Bergmann <mb@software-by-mabe.com>
2026-01-08 21:00:10 +0100

    updates.

--- StormCProjs/Planets/Planets.c
+++ StormCProjs/Planets/Planets.c
@@ -1,4 +1,5 @@
 #include <exec/types.h>
+#include <exec/tasks.h>
 
 #include <graphics/gfxbase.h>
 #include <intuition/intuition.h>
@@ -7,6 +8,7 @@
 #include <clib/graphics_protos.h>
 #include <clib/intuition_protos.h>
 #include <clib/dos_protos.h>
+#include <clib/alib_protos.h>
 
 #include <stdio.h>
 #include <math.h>
@@ -19,13 +21,17 @@ struct RastPort *rport = NULL;
 
 UWORD wWidth, wHeight, wCenterX, wCenterY;
 
+APTR tmpbuf = NULL;
+
 void cleanup() {
-	if(GfxBase) {
+	if(GfxBase)
 		CloseLibrary(GfxBase);
-	}
-	if(IntuitionBase) {
+	if(IntuitionBase)
 		CloseLibrary(IntuitionBase);
-	}
+	if(tmpbuf)
+		FreeVec(tmpbuf);
+	if(win)
+		CloseWindow(win);
 }
 
 void openLibs() {
@@ -99,33 +105,14 @@ void drawTrabant(LONG x, LONG y, LONG radius, LONG radTrabant, UWORD col) {
 	}
 }
 
-void handleMessages(BOOL *done) {
-	struct IntuiMessage *imsg;
-
-	while(imsg = (struct IntuiMessage*) GetMsg(win->UserPort)) {
-		printf("Received msg: %d\n", imsg->Code);
-		ReplyMsg((struct Message*)imsg);
-
-		switch (imsg->Class) {
-			case IDCMP_CLOSEWINDOW:
-				printf("Close gadget pressed...\n");
-				*done = TRUE;
-				break;
-			default:
-				printf("Unknown message!\n");
-				break;
-		}
-	}
-}
-
 #define MAXVECTORS 200
 struct AreaInfo areaInfo;
 struct TmpRas tmpRas;
 BYTE areabuf[MAXVECTORS*5];
-APTR tmpbuf = NULL;
-ULONG tmpbufSize = 0;
 
 void setupDrawEnv() {
+	ULONG tmpbufSize = 0;
+
 	wWidth = win->Width;
 	wHeight = win->Height;
 	wCenterX = wWidth/2;
@@ -140,44 +127,86 @@ void setupDrawEnv() {
 	printf("Init AreaInfo...done\n");
 
 	printf("Init TmpRas...\n");
-	tmpbuf = AllocMem(tmpbufSize, MEMF_CHIP);
+	tmpbuf = AllocVec(tmpbufSize, MEMF_CHIP | MEMF_CLEAR);
 	InitTmpRas(&tmpRas, tmpbuf, RASSIZE(wWidth, wHeight));
 	rport->TmpRas = &tmpRas;
 	printf("Init TmpRas...done\n");
 }
 
-void mainLoop() {
-	BOOL done = FALSE;
+void drawLoop() {
+	ULONG i = 0;
+	printf("Entering draw loop...\n");
 
-	printf("Entering main loop...\n");
+	//drawEllipse(wCenterX, wCenterY, 20, 20, TRUE, 2);
 
-	drawEllipse(wCenterX, wCenterY, 20, 20, TRUE, 2);
+	while(TRUE) {
+		printf("Draw loop...\n");
 
-	while (!done) {
-		//printf("Waiting for sigbit...\n");
-		//Wait (1L << win->UserPort->mp_SigBit);
-
-		drawTrabant(wCenterX, wCenterY, 50, 5, 1);
-
-		handleMessages(&done);
+		//drawTrabant(wCenterX, wCenterY, 50, 5, 1);
+		for(i = 0;i < 100000;i++)
 	}
 
-	printf("Exiting main loop.\n");
+	printf("Exiting draw loop.\n");
+}
+
+char *taskName = "PlanetsLoop";
+struct Task *task = NULL;
+ULONG STACK_SIZE = 4096L;
+BOOL appLoopDone = FALSE;
+
+void handleMessagesLoop() {
+	struct IntuiMessage *imsg;
+
+	printf("Handle messages loop...\n");
+
+	while(!appLoopDone) {
+		printf("Waiting for sigbit...\n");
+		Wait(1L << win->UserPort->mp_SigBit);
+
+		while(imsg = (struct IntuiMessage*) GetMsg(win->UserPort)) {
+			printf("Received msg: %d\n", imsg->Code);
+			ReplyMsg((struct Message*)imsg);
+
+			switch (imsg->Class) {
+				case IDCMP_CLOSEWINDOW:
+					printf("Close gadget pressed...\n");
+					appLoopDone = TRUE;
+					break;
+				default:
+					printf("Unknown message!\n");
+					break;
+			}
+		}
+	}
+
+	printf("Exiting handle messages loop.\n");
 }
 
 void main(int argc, char *argv[]) {
-	openLibs();
-	openWindow();
-	setupDrawEnv();
+	//openLibs();
+	//openWindow();
+	//setupDrawEnv();
 
-	mainLoop();
+	task = CreateTask(taskName, 0, drawLoop, STACK_SIZE);
+	if(!task) {
+		printf("Unable to create task!\n");
+		cleanup();
+		exit(1);
+	}
 
-	FreeMem(tmpbuf, tmpbufSize);
-	CloseWindow(win);
-	printf("Window closed.\n");
+	//handleMessagesLoop();
 
+	printf("Forbid...\n");
+	Forbid();
+	printf("Delete task...\n");
+	DeleteTask(task);
+	printf("Permit...\n");
+	Permit();
+
+	printf("Cleaning up...\n");
 	cleanup();
 
+	printf("Exiting...\n");
 	exit(0);
 }
 
